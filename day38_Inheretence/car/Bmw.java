a214324ba726c4db984c27f08d61ddd1bdc51491
gitfirstlesson <metin5153@gmail.com>
2022-12-14 11:45:58 +0000

    car

--- day38_Inheretence/car/Bmw.java
+++ day38_Inheretence/car/Bmw.java
@@ -1,4 +1,18 @@
 package day38_Inheretence.car;
 
-public class Bmw {
+public class Bmw extends Car {
+    public Bmw(String color, String model, int year, double price, int miles) {
+        super(color, model, "BMW", year, price, miles);
+    }
+
+    public void Breakdown(){
+        System.out.println(brand+model+"breaks down after 100k miles");
+    }
+
+    public void Racing(){
+
+        System.out.println(model+brand +"is racign");
+
+    }
+
 }
--- day38_Inheretence/car/Tesla.java
+++ day38_Inheretence/car/Tesla.java
@@ -1,4 +1,15 @@
 package day38_Inheretence.car;
 
-public class Tesla {
+public class Tesla extends Car{
+    public Tesla(String color, String model, int year, double price, int miles) {
+        super(color, model, "TESLA", year, price, miles);
+    }
+
+
+    public void autoPilot(){
+        System.out.println(brand+model+"is an autopilot mode");
+    }
+
+
+
 }
